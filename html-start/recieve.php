089e7338edd2b2c90f73c9d019302ce436760f65
Z-100 <marvin131t@gmail.com>
2021-04-30 16:40:06 +0200

    Delete html-start directory

--- html-start/recieve.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-
-    echo "<p>Vorname: " . $firstname . "</p>";
-    echo "<p>Nachname: " . $lastname . "</p>";
-    echo "<p>E-Mail: " . $email . "</p>";
